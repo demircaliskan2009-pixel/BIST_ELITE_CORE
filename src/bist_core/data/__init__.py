a1e257ceea2d88c77fb69434ca78f9d25ebf98e3
demircaliskan2009-pixel <demir.caliskan2009@gmail.com>
2025-11-29 13:02:02 +0300

    Update data __init__

--- src/bist_core/data/__init__.py
+++ src/bist_core/data/__init__.py
@@ -1,2 +1,33 @@
-from .ingest.local_csv import read_csv, register_dataset, load_registered_dataset
-__all__ = ["read_csv", "register_dataset", "load_registered_dataset"]
+from __future__ import annotations
+
+from .registry import (
+    DatasetMetadata,
+    DatasetRegistry,
+    DEFAULT_REGISTRY_ENV,
+    DEFAULT_REGISTRY_RELATIVE,
+    get_default_registry,
+)
+from .eod import (
+    DEFAULT_SNAPSHOT_ENV,
+    DEFAULT_SNAPSHOT_RELATIVE,
+    build_and_store_eod_snapshot,
+    build_snapshot_path,
+    get_default_snapshot_root,
+    read_eod_snapshot,
+    write_eod_snapshot,
+)
+
+__all__ = [
+    "DatasetMetadata",
+    "DatasetRegistry",
+    "DEFAULT_REGISTRY_ENV",
+    "DEFAULT_REGISTRY_RELATIVE",
+    "get_default_registry",
+    "DEFAULT_SNAPSHOT_ENV",
+    "DEFAULT_SNAPSHOT_RELATIVE",
+    "get_default_snapshot_root",
+    "build_snapshot_path",
+    "write_eod_snapshot",
+    "read_eod_snapshot",
+    "build_and_store_eod_snapshot",
+]
