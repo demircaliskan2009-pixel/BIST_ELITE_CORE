9e457576c3b8436128800b9d0a5a1f1bd53824ba
demircaliskan2009-pixel <demir.caliskan2009@gmail.com>
2026-04-05 17:04:36 +0300

    feat(edge): deterministic edge registry + validation hardening

--- src/bist_core/edge/__init__.py
+++ src/bist_core/edge/__init__.py
@@ -3,6 +3,18 @@
 from bist_core.edge.bucket_key import edge_bucket_key, regime_from_feat
 from bist_core.edge.edge_engine_v2 import EdgeEngineV2
 from bist_core.edge.edge_fusion import EdgeFusion
+from bist_core.edge.registry import (
+    EdgeCondition,
+    EdgeDefinition,
+    EdgeLogic,
+    EdgeRegistry,
+    EdgeRequiredData,
+    EdgeRiskProfile,
+    EdgeValidationResult,
+    build_builtin_edge_registry,
+    builtin_bist_edges,
+    validate_edge_definition,
+)
 from bist_core.edge.edge_store import EdgeStore
 from bist_core.edge.live_edge_buffer import LiveEdgeBuffer
 from bist_core.edge.live_edge_engine import LiveEdgeEngine
@@ -11,10 +23,20 @@ from bist_core.edge.multi_tf_edge import MultiTFEdge
 __all__ = [
     "EdgeEngineV2",
     "EdgeFusion",
+    "EdgeCondition",
+    "EdgeDefinition",
+    "EdgeLogic",
+    "EdgeRegistry",
+    "EdgeRequiredData",
+    "EdgeRiskProfile",
     "EdgeStore",
+    "EdgeValidationResult",
     "LiveEdgeBuffer",
     "LiveEdgeEngine",
     "MultiTFEdge",
+    "build_builtin_edge_registry",
+    "builtin_bist_edges",
     "edge_bucket_key",
     "regime_from_feat",
+    "validate_edge_definition",
 ]
